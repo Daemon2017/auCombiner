7786f929e90af4630193fca9ddbf3a2e359d462c
Daemon2017 <daemon2017@gmail.com>
2024-04-11 21:49:34 +0300

    Отмена переноса приложения в Web

--- back/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-apt-get update
-python3 -m pip install -r requirements.txt
--- back/process.py
+++ back/process.py
@@ -1,26 +1,19 @@
-from io import StringIO
+import os
 
 import numpy as np
 import pandas as pd
-from flask import Flask, request, Response
-from flask_cors import CORS
-from waitress import serve
-
-app = Flask(__name__)
-cors = CORS(app)
 
 data_dir = 'data'
 support_columns = ['RSID', 'CHROMOSOME', 'POSITION']
 
+dfs = []
+data_path = os.path.join(data_dir)
+for file in list(filter(lambda f: f.lower().endswith('.csv'), os.listdir(data_path))):
+    file_path = os.path.join(data_dir, file)
+    if os.path.isfile(file_path):
+        dfs.append(pd.read_csv(file_path, sep=',', dtype=str))
 
-@app.route('/combine', methods=['POST'])
-def combine():
-    dfs = []
-    files = request.files.getlist("csv_files")
-    for file in files:
-        csv_string = file.read().decode('utf-8')
-        dfs.append(pd.read_csv(StringIO(csv_string), sep=',', dtype=str))
-
+if len(dfs) > 1:
     merged_df = []
     for i, df in enumerate(dfs):
         if i == 0:
@@ -33,11 +26,6 @@ def combine():
 
     mode_df = merged_values_df.mode(axis=1)
     merged_support_df['RESULT'] = np.where(mode_df.count(axis=1) == 1, mode_df[0], '--')
-    return Response(merged_support_df.to_csv(sep=',', index=False, header=True), mimetype='text/csv')
-
-
-if __name__ == '__main__':
-    print('auCombiner ready!')
-    serve(app,
-          host='0.0.0.0',
-          port=8080)
+    merged_support_df.to_csv('combined.csv', sep=',', index=False, header=True)
+else:
+    print('There must be more than 1 data sets!')
